3caad489876d76a299ab63f66bc3ec45edf1d271
Fannon <heimlersimon@gmail.com>
2016-02-01 17:32:31 +0100

    * v1.0.0 * Complete rewrite of the extension. * There is only one substitution now, the <substitute> tag. Everything between will be substitutet. * If you need Random IDs, Increments etc. please consider using this extension in combination with the IDProvider extension

--- Substitutor.php
+++ Substitutor.php
@@ -33,15 +33,6 @@ $dirbasename = basename( $dir );
 
 
 //////////////////////////////////////////
-// CONFIG                               //
-//////////////////////////////////////////
-
-$wgSubstitutorMinRand          = 1000000000;
-$wgSubstitutorMaxRand          = 9999999999;
-$wgSubstitutorRandStringLength = 12;
-
-
-//////////////////////////////////////////
 // CREDITS                              //
 //////////////////////////////////////////
 
@@ -49,7 +40,7 @@ $wgExtensionCredits['other'][] = array(
    'path'           => __FILE__,
    'name'           => 'Substitutor',
    'author'         => array('Simon Heimler'),
-   'version'        => '0.1.0',
+   'version'        => '1.0.0',
    'url'            => 'https://www.mediawiki.org/wiki/Extension:Substitutor',
    'descriptionmsg' => 'substitutor-desc',
    'license-name'   => 'MIT'
@@ -62,8 +53,7 @@ $wgExtensionCredits['other'][] = array(
 //////////////////////////////////////////
 
 // Register hooks
-$wgHooks['PageContentSaveComplete'][] = 'onPageContentSaveComplete';
-
+$wgHooks['PageContentSave'][] = 'onPageContentSave';
 
 
 //////////////////////////////////////////
@@ -71,201 +61,39 @@ $wgHooks['PageContentSaveComplete'][] = 'onPageContentSaveComplete';
 //////////////////////////////////////////
 
 /**
-* Hook: After a wiki page is saved, look for strings to substitute
-* If there are some, a new revision will be made that contains the substitutions
+* Hook: After a wiki page is saved,
+* look for the <substitute> tag, parse its content and replace the complete tag with it.
 */
-function onPageContentSaveComplete( $wikiPage, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $status ) {
+function onPageContentSave( &$wikiPage, &$user, &$content, &$summary, $isMinor, $isWatch, $section, &$flags, &$status ) {
 
-  // Get all necessary variables
-  $oldText  = $content->getContentHandler()->serializeContent($content);
-  $title    = $wikiPage->getTitle();
-  $titleObj = Title::makeTitle(0, $title);
-  $page     = WikiPage::factory($titleObj);
-  $context  = new RequestContext();
+  global $wgOut;
 
-  // Do the actual substitution
-  $newText = substituteTimestamp($oldText);
-  $newText = substituteRandomNumber($newText);
-  $newText = substituteRandomString($newText);
-  $newText = substituteFakeID($newText);
-  $newText = substituteShortID($newText);
+  $re = "/<substitute>(.*?)<\\/substitute>/s";
 
+  $text = $content->getContentHandler()->serializeContent($content);
 
-  // If a substitution was made, save the edited page anew
-  // @see https://doc.wikimedia.org/mediawiki-core/master/php/html/classWikiPage.html
-  if ($oldText != $newText ) {
-    $content = $content->getContentHandler()->unserializeContent( $newText );
-    $page->doEditContent($content,
-      $context->getUser(),
-      "Extension:Substitor - automatic string substitution"
-    );
-  };
+  $newText = $text;
+
+  preg_match_all($re, $newText, $matches);
+
+  if ($matches[1]) {
+    foreach ($matches[1] as $match) {
+      $replacement = $wgOut->parseInline($match, false);
+
+      $completeMatch = '<substitute>' . $match . '</substitute>';
+
+      // Only replace the first instance of the match within the wikitext
+      // http://stackoverflow.com/a/1252710/776425
+      $pos = strpos($newText, $completeMatch);
+      if ($pos !== false) {
+        $newText = substr_replace($newText, $replacement, $pos, strlen($completeMatch));
+      }
+    }
+  }
+
+  if ( $newText != $text ) {
+      $content = $content->getContentHandler()->unserializeContent( $newText );
+  }
 
   return true;
-}
-
-
-/**
- * Substitutes ___TIMESTAMP___ for the current UNIX timestamp
- *
- * @param  [string] $oldText original mediawiki text
- * @return [string]          substituted mediawiki text
- */
-function substituteTimestamp($oldText) {
-
-  $date = new DateTime();
-  $timestamp = $date->getTimestamp();
-  $newText = str_replace("___TIMESTAMP___", $timestamp, $oldText);
-
-  return $newText;
-}
-
-/**
- * Substitutes ___RANDOMNUMBER___ with a random number
- * Minimum and maximum are defined through $wgSubstitutorMinRand and $wgSubstitutorMaxRand
- *
- * @param  [string] $oldText original mediawiki text
- * @return [string]          substituted mediawiki text
- */
-function substituteRandomNumber($oldText) {
-
-  $pattern = "/___RANDOMNUMBER___/";
-  $newText = preg_replace_callback($pattern, "generateRandomNumberCallback", $oldText);
-
-  return $newText;
-}
-
-/**
- * Substitutes ___RANDOMSTRING___ with a random string of length $wgSubstitutorRandStringLength
- *
- * @param  [string] $oldText original mediawiki text
- * @return [string]          substituted mediawiki text
- */
-function substituteRandomString($oldText) {
-
-  $pattern = "/___RANDOMSTRING___/";
-  $newText = preg_replace_callback($pattern, "generateRandomStringCallback", $oldText);
-
-  return $newText;
-}
-
-
-/**
- * Substitutes ___FAKEID___ with a fake ID, that should be unique
- * (No garantuees however!)
- *
- * @param  [string] $oldText original mediawiki text
- * @return [string]          substituted mediawiki text
- */
-function substituteFakeID($oldText) {
-
-  $pattern = "/___FAKEID___/";
-  $newText = preg_replace_callback($pattern, "generateFakeIDCallback", $oldText);
-
-  return $newText;
-}
-
-
-/**
- * Substitutes ___SHORTID___ with a shorter fake ID, that should be unique
- *
- * @param  [string] $oldText original mediawiki text
- * @return [string]          substituted mediawiki text
- */
-function substituteShortID($oldText) {
-
-  $pattern = "/___SHORTID___/";
-  $newText = preg_replace_callback($pattern, "generateShortIDCallback", $oldText);
-
-  return $newText;
-}
-
-
-
-//////////////////////////////////////////
-// HELPER / CALLBACK FUNCTIONS          //
-//////////////////////////////////////////
-
-/**
- * Callback function that returns a random number between $wgSubstitutorMinRand and $wgSubstitutorMaxRand
- *
- * @return [integer]
- */
-function generateRandomNumberCallback() {
-
-  global $wgSubstitutorMinRand;
-  global $wgSubstitutorMaxRand;
-
-  return rand($wgSubstitutorMinRand, $wgSubstitutorMaxRand);
-}
-
-/**
- * Callback function that returns a random string with length of $wgSubstitutorRandStringLength
- *
- * @return [string]
- */
-function generateRandomStringCallback() {
-
-  global $wgSubstitutorRandStringLength;
-
-  return generateRandomString($wgSubstitutorRandStringLength);
-}
-
-
-
-/**
- * Generates a Fake ID that is very likely to be truly unique (no guarantee however!)
- *
- * This is achived through mixing a militimestamp (php uniqid();) with a random string
- *
- * @return [string]
- */
-function generateFakeIDCallback() {
-
-  $id = generateRandomString(4);
-  $id .= uniqid();
-
-  return $id;
-}
-
-/**
- * Generates a Fake ID that is very likely to be truly unique (no guarantee however!)
- * This version tries to be as short as possible
- *
- * This is achived through mixing a militimestamp (php uniqid();) with a random string
- *
- * @return [string]
- */
-function generateShortIDCallback() {
-
-  // Generates a random string of length 1-2.
-  $id = base_convert(rand(0, 36^2), 10, 36);
-
-  // This will "compress" the uniqid (some sort of microtimestamp) to a more dense string
-  $id .= base_convert(uniqid(), 10, 36);
-
-  return $id;
-}
-
-/**
- * Rturns a random string with length of $length
- *
- * @param  [number] $length string length
- * @return [string]
- */
-function generateRandomString($length) {
-
-  global $wgSubstitutorRandStringLength;
-
-  if (!$length) {
-    $length = $wgSubstitutorRandStringLength;
-  }
-
-  $key = '';
-  $keys = array_merge(range(0,9), range('a', 'z'));
-
-  for($i=0; $i < $length; $i++) {
-      $key .= $keys[array_rand($keys)];
-  }
-  return $key;
 }
